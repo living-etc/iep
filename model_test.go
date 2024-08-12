0bee13eb1304621520b13a7b4a6f8a46a6ca39bf
Chris Wilson <hey@chris-wilson.me>
2024-08-12 19:48:24 +0100

    Add non-working model test
    Tryed adding testest to test the model. Couldn't get it working and
    don't want to spend time looking into it now. Gonna keep on working on
    the UI then come back to this later.

--- model_test.go
+++ model_test.go
@@ -8,37 +8,47 @@ import (
 )
 
 func TestNewModel(t *testing.T) {
-	tests := []struct {
+	testcases := []struct {
+		name       string
 		keystrokes []tea.KeyMsg
-		model      Model
+		modelWant  Model
 	}{
 		{
-			model: Model{
-				cursor: 1,
-			},
+			name:      "j",
+			modelWant: Model{cursor: 1},
 			keystrokes: []tea.KeyMsg{
-				{Type: tea.KeyRunes, Runes: []rune{'j'}},
+				{Type: tea.KeyRunes, Runes: []rune("j")},
+			},
+		},
+		{
+			name:      "j-k",
+			modelWant: Model{cursor: 0},
+			keystrokes: []tea.KeyMsg{
+				{Type: tea.KeyRunes, Runes: []rune("j")},
+				{Type: tea.KeyRunes, Runes: []rune("k")},
 			},
 		},
 	}
 
 	testModel := teatest.NewTestModel(t, NewModel())
 
-	for _, tt := range tests {
-		for _, keystroke := range tt.keystrokes {
-			testModel.Send(keystroke)
-		}
+	for _, testcase := range testcases {
+		t.Run(testcase.name, func(t *testing.T) {
+			for _, keystroke := range testcase.keystrokes {
+				testModel.Send(keystroke)
+			}
 
-		testModel.Send(tea.KeyMsg{
-			Type:  tea.KeyRunes,
-			Runes: []rune("q"),
+			testModel.Send(tea.KeyMsg{
+				Type:  tea.KeyRunes,
+				Runes: []rune("q"),
+			})
+
+			modelGot := testModel.FinalModel(t).(Model)
+			modelWant := testcase.modelWant
+
+			if modelGot.cursor != modelWant.cursor {
+				t.Fatalf("want: %v, got: %v", modelWant.cursor, modelGot.cursor)
+			}
 		})
-
-		modelGot := testModel.FinalModel(t).(Model)
-		modelWant := tt.model
-
-		if modelGot.cursor != modelWant.cursor {
-			t.Fatalf("want: %v, got: %v", modelWant.cursor, modelGot.cursor)
-		}
 	}
 }
