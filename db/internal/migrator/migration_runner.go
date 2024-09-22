37b0c0008f241005c286c7a69455611f1fdd757b
Chris Wilson <hey@chris-wilson.me>
2024-09-22 16:35:00 +0100

    Implement migration runner

--- db/internal/migrator/migration_runner.go
+++ db/internal/migrator/migration_runner.go
@@ -3,6 +3,8 @@ package migrator
 import (
 	"context"
 	"database/sql"
+	"fmt"
+	"os"
 )
 
 type MigrationRunner struct{}
@@ -12,5 +14,24 @@ func (r *MigrationRunner) Run(
 	db *sql.DB,
 	migration Migration,
 ) error {
+	fmt.Fprintf(os.Stdout, "Running migrations...\n")
+	fmt.Fprintf(os.Stdout, "\t%s\n", migration.Id)
+	fmt.Fprintf(os.Stdout, "Finished migrations\n")
+
+	exec(ctx, db, string(migration.Statement))
+
+	add_migration_id_statement := fmt.Sprintf(migration.Statement, migration.Id)
+	exec(ctx, db, add_migration_id_statement)
+
 	return nil
+}
+
+func exec(ctx context.Context, db *sql.DB, statement string, args ...any) sql.Result {
+	res, err := db.ExecContext(ctx, statement, args...)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to execute statement %s: %s", statement, err)
+		os.Exit(1)
+	}
+
+	return res
 }
