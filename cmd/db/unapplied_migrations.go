ffbeefb05a53df2c5123c7a9678282468a316f24
Chris Wilson <hey@chris-wilson.me>
2024-09-22 21:54:16 +0100

    Simplify unapplied migrations

--- cmd/db/unapplied_migrations.go
+++ cmd/db/unapplied_migrations.go
@@ -3,8 +3,6 @@ package main
 import (
 	"context"
 	"database/sql"
-	"fmt"
-	"os"
 	"sort"
 
 	"iep/cmd/db/migrations"
@@ -27,59 +25,17 @@ func Get(
 	sort.Strings(migration_ids)
 
 	for _, migration_id := range migration_ids {
-		if !migration_completed(
-			migration_id,
-			completed_migrations(ctx, db),
-		) {
-			unapplied_migrations = append(
-				unapplied_migrations,
-				MigrationFunctionRegistry[migration_id](),
-			)
+
+		var migration string
+		if err := db.QueryRow("SELECT id FROM migrations WHERE id = ?", migration_id).Scan(&migration); err != nil {
+			if err == sql.ErrNoRows {
+				unapplied_migrations = append(
+					unapplied_migrations,
+					MigrationFunctionRegistry[migration_id](),
+				)
+			}
 		}
 	}
 
 	return unapplied_migrations
-}
-
-func migration_completed(migration_file string, completed_migrations []string) bool {
-	for _, completed_migration := range completed_migrations {
-		if completed_migration == migration_file {
-			return true
-		}
-	}
-
-	return false
-}
-
-func completed_migrations(ctx context.Context, db *sql.DB) []string {
-	completed_migration_rows := query(ctx, db, getMigrationIdsSQL)
-	defer completed_migration_rows.Close()
-
-	completed_migrations := []string{}
-	for completed_migration_rows.Next() {
-		var migration string
-
-		if err := completed_migration_rows.Scan(&migration); err != nil {
-			fmt.Fprintf(os.Stderr, "Scan err: %s", err)
-			os.Exit(1)
-		}
-
-		completed_migrations = append(completed_migrations, migration)
-	}
-	if err := completed_migration_rows.Err(); err != nil {
-		fmt.Fprintf(os.Stderr, "Row err: %s", err)
-		os.Exit(1)
-	}
-
-	return completed_migrations
-}
-
-func query(ctx context.Context, db *sql.DB, statement string, args ...any) *sql.Rows {
-	res, err := db.QueryContext(ctx, statement, args...)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "failed to query statement %s: %s", statement, err)
-		os.Exit(1)
-	}
-
-	return res
 }
